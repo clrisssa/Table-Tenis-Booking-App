7315381d24d671483793e97fde6051454879819b
Clarissa Poedjiono <its.clarissa@gmail.com>
2017-04-22 16:09:44 +0800

    Created Project

---
+++ WebApplication2/src/java/MODEL/Employee.java
@@ -0,0 +1,49 @@
+package MODEL;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Clarissa Poedjiono
+ */
+public class Employee {
+    private String username;
+    private String password;
+    private String name;
+    
+    public Employee (String username, String password, String name){
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
